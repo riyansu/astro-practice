bb776e154a528d62ba4537045e24a1bfc6e694b9
Riyansu <kenKEN.DEM@gmail.com>
2024-02-11 22:33:43 +0900

    test ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  output:"server",
-  integrations: [solidJs(), tailwind()]
+  output: "server",
+  integrations: [solidJs(), tailwind()],
+  adapter: cloudflare()
 });
