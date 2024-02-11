927c89a2a84da80495f88c36ec75437fe6f92daa
Riyansu <kenKEN.DEM@gmail.com>
2024-02-11 19:04:36 +0900

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config';
 
-import solid from "@astrojs/solid-js";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid()]
+  integrations: [solidJs()]
 });
